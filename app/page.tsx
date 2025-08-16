8cb56d91f8278e9fdf3ebb274b9c6c25faa37d1a
Bogale Demas <bogidemas@gmail.com>
2025-08-17 01:33:27 +0300

    folder sturacture & Home page implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,89 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import heroPhoto from "./../public/images/hero.png";
 export default function Home() {
-  return <main>PORTFOLIO</main>;
+  const handleCvDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/bogicv.pdf";
+    link.download = "bogicv.pdf";
+    link.click();
+  };
+  return (
+    <main className="flex dark:bg-slate-950/90 items-center py-8 flex-col justify-center relative min-h-screen">
+      <div className="max-w-[1080px]  grid lg:grid-cols-2">
+        <div className="flex-1 flex flex-col px-4 container mx-auto">
+          <div className="w-full flex items-start">
+            <p className="title">Hi! I&apos;m Bogale</p>
+          </div>
+          <div className="flex-1 py-4 flex flex-col justify-end">
+            <h5 className="subtitle">Software developer</h5>
+            <p className="py-4 ">
+              Highly skilled software developer with extensive experience in
+              building robust web and mobile applications.
+            </p>
+            <p className="py-4 ">
+              {" "}
+              A collaborative team player with excelent problem-solviing
+              capablity and a passion for continous learning and adapting to
+              emerging technologies.
+            </p>
+            <div className="text-sm text-[#342c3d76]">
+              <p className="text-blue-500 font-semibold">
+                Email:{" "}
+                <Link href={"bogidemas@gmail.com"}>bogidemas@gmail.com</Link>
+              </p>
+              <p className="font-semibold ">Tel: +251923872187</p>
+            </div>
+            <div className="py-4 flex gap-4">
+              <button
+                className="w-full border p-3  text-lg border-blue-300 bg-blue-600 rounded-md font-semibold"
+                onClick={handleCvDownload}
+              >
+                Download cv
+              </button>
+              <button className="w-full font-semibold border text-lg p-3  border-gray-300  rounded-md">
+                Hire me know!
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 flex items-center mx-auto justify-center w-full rounded-full p-10 max-w-[540px]">
+          <Image src={heroPhoto} alt="hero" className="rounded-lg" />
+        </div>
+      </div>
+      <div className="w-full container">
+        <h5 className="underline font-bold text-center text-3xl my-8 text-[#342c3d76]">
+          Services
+        </h5>
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-4 gap-2">
+          {/* {[
+            {
+              title: "Backend development",
+              discription:
+                "Develop backend applications using nodejs, express with sql and non sql",
+              icon: "/images/backend.png",
+              link: "/projects/?filter=backend",
+            },
+            {
+              title: "Frontend development",
+              discription:
+                "Develop frontend applications using reactjs,tailwindcss, styled components.",
+              icon: "/images/frontend.jpeg",
+              link: "/projects/?filter=frontend",
+            },
+            {
+              title: "Mobile app development",
+              discription:
+                "Develop amaizing crose plateform mobile application using React netive",
+              icon: "/images/android.png",
+              link: "/projects/?filter=mobile",
+            },
+          ].map((Item, index) => (
+            <ServiceCard key={index} {...Item} />
+          ))} */}
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,74 @@
+"use client";
+import Image from "next/image";
+
+import Link from "next/link";
+import { useState } from "react";
+import Icon from "./Icon";
+import Theme from "./Theme";
+
+const Header = () => {
+  const navLinks = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Projects",
+      link: "/projects",
+    },
+    {
+      name: "About",
+      link: "/about",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+  ];
+  const [dispBlockNav, setDispBlockNav] = useState(false);
+  return (
+    <div className="flex items-center justify-center relative dark:bg-slate-950">
+      <nav className="max-w-[1080px] navbar flex w-full gap-2 shadow max-sm:flex-col sm:items-center dark:bg-slate-950 bg-white">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex items-center justify-between max-sm:w-full">
+            <Link href="/" aria-label="Homepage Link">
+              <Image
+                src="/images/logo.png"
+                width={60}
+                height={60}
+                className="flex items-center justify-center"
+                alt=""
+              />
+            </Link>
+            <div className="sm:hidden">
+              <button
+                type="button"
+                onClick={() => setDispBlockNav(!dispBlockNav)}
+              >
+                {!dispBlockNav ? (
+                  <Icon className="icon-[tabler--menu-2]" />
+                ) : (
+                  <Icon className="icon-[tabler--x]" />
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={`${!dispBlockNav && "hidden"} sm:block`}>
+          <ul className="flex flex-col sm:flex-row gap-2 p-0">
+            {navLinks.map(({ link, name }) => (
+              <Link key={link} href={link}>
+                <li className="font-semibold dark:text-slate-400 transition-all duration-300 text-[#342c3d76] border border-slate-300 dark:border-slate-700 hover:blur-[1px] p-2">
+                  {name}
+                </li>
+              </Link>
+            ))}
+            {<Theme />}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
